975e59457671ee8849d5fc2f627ad0a2e1302a33
ganesh1172 <ganesh152595@gmail.com>
2020-05-05 10:24:35 +0530

    simple array program to calculate total

---
+++ simple-array-sum.js
@@ -0,0 +1,10 @@
+/*
+ * Complete the simpleArraySum function below.
+ */
+function simpleArraySum(ar) {
+    var sum = 0;
+    for (var i = 0; i < ar.length; i++) {
+        sum += ar[i];
+    }
+    return sum;
+}
